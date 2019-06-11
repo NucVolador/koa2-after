0d3a651f92ac4fa3ed76cf20bed41be8f0bc2e35
yurong.du <yurong.du@ly.com>
2019-06-11 14:27:35 +0800

    完成

--- routes/queryJob.js
+++ routes/queryJob.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')()
 const Job = require("../dbs/models/job");
+const ApplyList = require("../dbs/models/applyList");
 const AdminCompanyInfo = require("../dbs/models/company");
 
 
@@ -73,36 +74,6 @@ router.get('/queryJob', async (ctx, next) =>{
 								result
 							}
 						})
-					// let obj_promise = new Promise(async (res,rej)=>{
-					// 	let result_temp = await temp_a.map(async (item,index)=>{
-					// 		// console.log(item,"这是职位")
-					// 		let AdminCompany_result = await AdminCompanyInfo
-					// 			.find({ownerId:item.ownerId})
-					// 			.select('address companyName email introduce phoneNum special avatar')
-					// 			// .then(AdminCompany_result =>{
-					// 			// 	item.company = AdminCompany_result[0]
-					// 			// 	// console.log(item,"这是item")
-					// 			// 	// console.log(AdminCompany_result[0],"这是职位");
-					// 			// })
-					// 		// console.log(AdminCompany_result,"AdminCompany_result")
-					// 		item.company = AdminCompany_result[0]
-					// 		return item;
-					// 	})
-					// 	// console.log(result_temp,"result_temp");
-					// 	res(result_temp)
-					// })
-					// ctx.body = {
-					// 	msg: "aa"
-					// }
-					//  await obj_promise.then(async data =>{
-					// 	 await setTimeout(()=>{
-					// 	 	console.log(data,"data")
-					//
-					// 	 },300);
-					//
-					//  })
-					// console.log(result_temp);
-					
 				}else{
 					result = {
 						item_list,
@@ -126,8 +97,61 @@ router.get('/queryJob', async (ctx, next) =>{
 	
 })
 
-router.get('/bar', function (ctx, next) {
-	ctx.body = 'this is a users/bar response'
+router.get('/queryState', async (ctx, next) =>{
+	let req = ctx.request.query;
+	let {
+		userId,
+		page
+	} = req;
+	let msg,code,result;
+	page = page?Number(page):1;
+	let item_list = [];
+	let page_size = 10;
+	let total_count = 0;
+	try {
+		await ApplyList
+			.find({candidateId: userId})
+			.then(async ApplyList_result=>{
+				total_count = ApplyList_result.length;
+			})
+		await ApplyList
+			.find({
+				candidateId: userId,
+			})
+			.skip(page_size* (page-1))
+			.limit(page_size)
+			.sort({ timestamp: -1 })
+			.then(async ApplyList_result=>{
+				if(ApplyList_result.length > 0){
+					item_list= ApplyList_result;
+					code = 1;
+					msg = "投递信息获取成功";
+					result = {
+						item_list,
+						page,
+						page_size,
+						total_count
+					}
+				}else{
+					result = {
+						item_list,
+						page,
+						page_size,
+						total_count
+					};
+					msg = "投递信息暂无";
+					code = 1
+				}
+			})
+	}catch (e) {
+		console.log(e);
+	}
+	
+	ctx.body={
+		msg,
+		code,
+		result
+	}
 })
 
 module.exports = router
